fd61530f5a0f5600101047be22021a01e40e46b5
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-10-16 17:24:46 +0800

    fix panics

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,18 @@ use dreamcore_image_processor::transformation::eyes::{Eyeball, Eyeballs};
 use dreamcore_image_processor::transformation::text::DreamcoreStyledTextTransform;
 use dreamcore_image_processor::transformation::{ImageTransformation, Pipeline};
 use futures::future::join_all;
-use std::sync::Arc;
-use std::time::Instant;
 use image::GenericImageView;
-use log::info;
+use log::{error, info};
+use std::sync::Arc;
+use std::time::{Duration, Instant};
 use tokio::task::spawn_blocking;
+use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
 
-    let provider = Arc::new(PinterestProvider::new("dreamcore background"));
+    let provider = Arc::new(PinterestProvider::new("dreamcore landscape"));
 
     let pipeline = Pipeline::default()
         + DreamcoreStyledTextTransform::default()
@@ -28,18 +29,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let now = Instant::now();
 
-    let tasks = (0..300).map(|i| {
+    let tasks = (0..1024).map(|i| {
         let pipeline = pipeline.clone();
         let provider = provider.clone();
 
         async move {
-            let mut img = provider.fetch_background().await.unwrap();
-            
+            let mut img = {
+                loop {
+                    match provider.fetch_background().await {
+                        Ok(img) => break img,
+                        Err(err) => {
+                            error!("Failed to fetch background image: {err}, sleeping for 1 second");
+                            sleep(Duration::from_secs(1)).await;
+                        }
+                    }
+                }
+            };
+
             let (w, h) = img.dimensions();
             info!("Resizing image {i} from {w}x{h} to 512x512");
-            
+
             crop_and_resize(&mut img, 512);
-            
+
             info!("Transforming image {i}");
 
             let img = spawn_blocking(move || {
